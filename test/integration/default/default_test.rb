237e94f41d201d322f1da9b6f9238d2e18919efa
mikhailyesman <43871862+mikhailyesman@users.noreply.github.com>
2018-11-28 23:32:56 +0300

    Update default_test.rb

--- test/integration/default/default_test.rb
+++ test/integration/default/default_test.rb
@@ -5,12 +5,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
-unless os.windows?
-  # This is an example test, replace with your own test.
-  describe user('root'), :skip do
-    it { should exist }
-  end
+describe package("docker-ce") do
+  it { should be_installed } 
 end
+
 
 # This is an example test, replace it with your own test.
 describe port(80), :skip do
