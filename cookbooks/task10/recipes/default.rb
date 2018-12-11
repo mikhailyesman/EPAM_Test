5392fc65706fb126e78311e01d2205c78b8d8b7d
mikhailyesman <43871862+mikhailyesman@users.noreply.github.com>
2018-12-11 19:22:16 +0300

    Update default.rb

--- cookbooks/task10/recipes/default.rb
+++ cookbooks/task10/recipes/default.rb
@@ -17,14 +17,63 @@ yum_package 'docker-ce' do
   action :install
 end
 
-file '/etc/docker/daemon.json' do
-        content '{ "insecure-registries" : ["192.168.10.111:5000" ] }'
-        owner 'root'
-        group 'root'
-        mode '0755'
-        action :create_if_missing
-end
-
 service 'docker' do
   action [:enable, :start]
 end
+
+if node.attribute?( 'vers' )
+  puts "version: #{node['vers']}"
+else
+  puts 'Wrong vers'
+end
+
+begin
+  h = node['host']
+  p1 = node['port1']
+  p2 = node['port2']
+
+  p_lst = {}
+  [ p1, p2 ].each do |p|
+    begin
+      Timeout.timeout(4) do
+        Socket.tcp( h, p ){}
+      end
+      p_lst[p] = true
+      puts "port #{p} is listening"
+    rescue 
+      p_lst[p] = false
+      puts "port #{p} is free"
+    end
+  end
+
+  
+  
+  p_start = !p_lst[p1] ? p1 : !p_lst[p2] ? p2 : 0
+  p_stop = p_lst[p1] ? port1 : p_lst[p2] ? p2 : 0
+  if p_start != 0
+    puts "Will be start service on #{p_start}"
+    if p_stop != 0
+      puts "Will be stop service on #{p_stop}"
+    end
+  else
+    puts "Error define service ports"
+  end
+
+  
+  
+  node.override['port_start'] = p_start if p_start != 0
+  node.override['port_stop'] = p_stop if p_stop != 0
+end
+
+
+
+execute 'service_start' do
+  command "docker run -d -p #{node['port_start']}:8080 --name #{node['branch']}_#{node['port_start']} #{node['server_ip']}:5000/#{node['branch']}:#{node['vers']}"
+  only_if { node.attribute?('port_start') }
+end
+
+execute 'service_stop' do
+  command "docker stop #{node['branch']}_#{node['port_stop']} && docker rm #{node['branch']}_#{node['port_stop']}"
+  only_if { node.attribute?('port_stop') }
+end
+
