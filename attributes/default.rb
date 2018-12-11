5c2f6641b7001232c3fe348abf1510c290cadec8
mikhailyesman <43871862+mikhailyesman@users.noreply.github.com>
2018-12-11 19:15:03 +0300

    Added data

--- attributes/default.rb
+++ attributes/default.rb
@@ -3,6 +3,6 @@ node.default['branch']='task10'
 node.default['host']='localhost'
 node.default['port1']=8080
 node.default['port2']=8081
-node.default['vers']='1.0.11'
+node.default['vers']='1.0.1'
 node.default['server_ip']='192.168.10.111'
 
